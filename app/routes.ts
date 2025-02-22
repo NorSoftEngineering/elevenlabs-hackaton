d94f6b2fae2876baf96920a6348a4e8b8d5d9741
Alexis Faure <devalexisfaure@gmail.com>
2025-02-22 14:27:48 +0100

    feat: add login page

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,8 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes';
 // Define your routes with type safety
 export default [
 	index('./screens/home.tsx'),
+	route('login', './screens/login.tsx'),
+
 	route('edge', './screens/edge.tsx'),
 	route('edgestream', './screens/edgestream.tsx'),
 	route('node', './screens/node.tsx'),
